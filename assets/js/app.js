8a6286fbbf71666e9f68ca976e9298d880b69347
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:22:49 +0100

    Adding react and typescript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+const _css = require("../css/app.scss");
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,4 +12,10 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Greeter from "./hello";
+
+const root = document.getElementById("root");
+ReactDOM.render(<Greeter name="Phoenix" />, root);
