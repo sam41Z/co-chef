44517897de0331bd1991dab5556b249bf102f5af
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:22:49 +0100

    extract api requests

--- assets/js/ingredients/ingredients_api.tsx
+++ assets/js/ingredients/ingredients_api.tsx
@@ -1,31 +1,14 @@
+import { post, get } from "../api_request.tsx";
+
 export interface Ingredient {
   id?: number,
   name: string
-}
+};
 
 export function postIngredient (ingredient: Ingredient) {
-  return fetch(
-    `http://localhost:4000/api/ingredients`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify({ingredient: ingredient})
-    }
-  )
-  .then(res => res.json())
-  .catch(error => console.log(error));
-}
+  return post('/api/ingredients', {ingredient: ingredient});
+};
 
 export function getIngredients () {
-  return fetch(
-      `http://localhost:4000/api/ingredients`,
-      {
-        method: "GET"
-      }
-    )
-    .then(res => res.json())
-    .then(res => res.data)
-    .catch(error => console.log(error));
-}
+  return get('/api/ingredients');
+};
--- assets/js/root.tsx
+++ assets/js/root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import IngredientList from "./ingredients/ingredient_list.tsx" 
-import IngredientForm from "./ingredients/ingredient_form.tsx"
+import IngredientList from "./ingredients/ingredient_list.tsx";
+import IngredientForm from "./ingredients/ingredient_form.tsx";
 
 const Root: React.FC<{}> = (props: {}) => {
   return (
